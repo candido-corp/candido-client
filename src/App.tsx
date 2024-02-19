bf05537e96c13d7db01655454645ceb05ffcc648
Marco Lodovici <mlodovici@alpenite.com>
2024-02-19 23:42:29 +0100

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button onClick={() => console.log('Ciao')}>Click me</Button>
+    </>
+  );
+}
+
+export default App;
