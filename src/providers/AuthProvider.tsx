a220f338cd48c9445968a29b971c830b992231fb
Marco Tasca <tascamarcoo@gmail.com>
2024-07-04 17:22:38 +0200

    feat: Add AuthProvider with context

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,32 +1,65 @@
-import { createContext, useContext, useState } from 'react';
+import React, { createContext, useState, ReactNode } from 'react';
 import { EnumUserPermissions, EnumUserRoles } from '@/models/enums/EnumUsers.ts';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
-const AuthContext = createContext({});
-
-interface User {
-  email: string | null;
-  roles: EnumUserRoles | null;
-  permissions: EnumUserPermissions | null;
+export interface JwtPayload {
+  sub: string;
+  roles: EnumUserRoles;
+  permissions: EnumUserPermissions;
 }
 
-export const AuthProvider = ({ children }: { children: never }) => {
-  const [user, setUser] = useState<User>();
+export const getAccessToken = (): User | null => {
+  const accessToken: string | undefined = Cookies.get('access_token');
+  if (!accessToken) {
+    return null;
+  }
+  const decoded = jwtDecode<JwtPayload>(accessToken);
+  return {
+    email: decoded.sub,
+    roles: decoded.roles,
+    permissions: decoded.permissions,
+  };
+};
 
-  const login = (userData: User) => {
-    setUser(userData);
+export const isAuthenticated = () => {
+  return !!getAccessToken();
+}
+
+export interface User {
+  email: string;
+  roles: EnumUserRoles;
+  permissions: EnumUserPermissions;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  isAuthenticated: boolean;
+  login: (userData: User) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  console.log(getAccessToken());
+
+  const [user, setUser] = useState<User | null>(getAccessToken());
+  const isAuthenticated = !!user;
+
+  const login = () => {
+    setUser(getAccessToken());
+    console.log(isAuthenticated);
   };
 
   const logout = () => {
-    setUser({ email: null, roles: null, permissions: null });
+    setUser(null);
+    console.log(isAuthenticated);
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
-};
-
-export const useAuth = () => {
-  return useContext(AuthContext);
 };
