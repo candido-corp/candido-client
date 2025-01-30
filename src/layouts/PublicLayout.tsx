6538cf2962dae8070f6e0d0e7e2c48c538d21ae7
Marco Lodovici <marcolodo98@gmail.com>
2025-01-30 23:34:26 +0100

    feat(layout): remove unused AppSidebarProvider from PublicLayout

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom';
 const PublicLayout = () => {
   return (
     <>
-      {/* <AppSidebarProvider> */}
       <Header />
       <main className="h-full">
         <Outlet />
