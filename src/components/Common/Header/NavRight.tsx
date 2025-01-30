771965c6d79d8bcb050ba8e47372b61f70b7bd73
Marco Lodovici <marcolodo98@gmail.com>
2025-01-30 23:32:08 +0100

    feat(header): add button for creating opportunities in NavRight component

--- src/components/Common/Header/NavRight.tsx
+++ src/components/Common/Header/NavRight.tsx
@@ -1,10 +1,21 @@
 import { BaseFC } from '@/models/interfaces/BaseFC';
 import { cn } from '@/utils/shadcn';
 import UserAvatar from './UserAvatar';
+import { Button } from '@/components/ui/button';
+import { NavLink } from 'react-router-dom';
+import { EnumRoutes } from '@/models/enums/EnumRoutes';
+import { Plus } from 'lucide-react';
+import { ModeToggle } from '../ModeToggle';
 
 const NavRight: React.FC<BaseFC> = ({ className }) => {
   return (
-    <nav className={cn('flex items-center gap-2', className)}>
+    <nav className={cn('flex items-center gap-4', className)}>
+      <Button asChild className="hidden md:flex">
+        <NavLink to={EnumRoutes.FORMS_CREATE}>
+          <Plus />
+          Build your opportunity
+        </NavLink>
+      </Button>
       <UserAvatar />
       <ModeToggle />
     </nav>
