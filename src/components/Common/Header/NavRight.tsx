52aa5363d30e5f1b0c5587cc12afb7b3b02472a2
Marco Lodovici <marcolodo98@gmail.com>
2025-03-10 19:11:18 +0100

    feat(header): update button text and reorder imports in NavRight component

--- src/components/Common/Header/NavRight.tsx
+++ src/components/Common/Header/NavRight.tsx
@@ -1,11 +1,10 @@
+import { Button } from '@/components/ui/button';
+import { EnumRoutes } from '@/models/enums/EnumRoutes';
 import { BaseFC } from '@/models/interfaces/BaseFC';
 import { cn } from '@/utils/shadcn';
-import UserAvatar from './UserAvatar';
-import { Button } from '@/components/ui/button';
-import { NavLink } from 'react-router-dom';
-import { EnumRoutes } from '@/models/enums/EnumRoutes';
 import { Plus } from 'lucide-react';
-import { ModeToggle } from '../ModeToggle';
+import { NavLink } from 'react-router-dom';
+import UserAvatar from './UserAvatar';
 
 const NavRight: React.FC<BaseFC> = ({ className }) => {
   return (
@@ -13,11 +12,10 @@ const NavRight: React.FC<BaseFC> = ({ className }) => {
       <Button asChild className="hidden md:flex">
         <NavLink to={EnumRoutes.FORMS_CREATE}>
           <Plus />
-          Build your opportunity
+          Create your own
         </NavLink>
       </Button>
       <UserAvatar />
-      <ModeToggle />
     </nav>
   );
 };
