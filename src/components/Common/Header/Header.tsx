bd90cfa3a8d6308c1ea43089ed6f312e92af8e0c
Marco Lodovici <marcolodo98@gmail.com>
2025-04-20 20:44:38 +0200

    refactor(Header): remove unused SVG and simplify Candido label

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -30,19 +30,7 @@ const Header: React.FC<HeaderProps> = ({ hasSidebar }) => {
               className="mr-4 flex items-center gap-2 lg:mr-6"
               end
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-6 w-6"
-              >
-                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-              </svg>
-              <span className="hidden font-bold lg:inline-block">Candido</span>
+              <span className="font-bold text-primary">Candido</span>
               <Separator
                 orientation="vertical"
                 className="ml-2 hidden h-4 md:flex"
