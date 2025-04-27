c061cc2403c957c4e695b1acb7721b4dbaabfcd3
Marco Lodovici <marcolodo98@gmail.com>
2025-04-28 00:48:08 +0200

    add missing visible parameter

--- src/config/navigation/types.ts
+++ src/config/navigation/types.ts
@@ -1,14 +1,14 @@
+import { EnumNavigationPlugin } from '@/config/navigation/enums/EnumNavigationPlugin.ts';
+import { EnumNavigationVisibility } from '@/config/navigation/enums/EnumNavigationVisibility.ts';
 import { LucideIcon } from 'lucide-react';
-import {EnumNavigationPlugin} from "@/config/navigation/enums/EnumNavigationPlugin.ts";
-import {EnumNavigationVisibility} from "@/config/navigation/enums/EnumNavigationVisibility.ts";
 
 /**
  * A plugin represents a navigation module (e.g., dashboard, forms).
  */
 export type NavigationPlugin = {
-    id: string;
-    navbar: FlatNavItem;
-    sidebar?: FlatNavItem[];
+  id: string;
+  navbar: FlatNavItem;
+  sidebar?: FlatNavItem[];
 };
 
 /**
@@ -16,34 +16,35 @@ export type NavigationPlugin = {
  * Can later be transformed into a tree via parentId.
  */
 export type FlatNavItem = {
-    id: string;
-    parentId?: string;
-    title: string;
-    url: string;
-    visibility?: EnumNavigationVisibility[];
-    icon?: LucideIcon;
-    roles?: string[];
-    badgeCount?: number | (() => number);
+  id: string;
+  parentId?: string;
+  title: string;
+  url: string;
+  visibility?: EnumNavigationVisibility[];
+  icon?: LucideIcon;
+  roles?: string[];
+  badgeCount?: number | (() => number);
+  visible?: boolean | (() => boolean);
 };
 
 /**
  * FlatNavItem with active state, used in rendering.
  */
 export type FlatNavItemWithActive = FlatNavItem & {
-    isActive: boolean;
+  isActive: boolean;
 };
 
 /**
  * NavItem with recursive children, used in rendering.
  */
 export type NavItem = FlatNavItem & {
-    children?: NavItem[];
+  children?: NavItem[];
 };
 
 /**
  * Enum for navigation plugins.
  */
 export type RouteHandle = {
-    pluginId: EnumNavigationPlugin;
-    [key: string]: any;
+  pluginId: EnumNavigationPlugin;
+  [key: string]: any;
 };
