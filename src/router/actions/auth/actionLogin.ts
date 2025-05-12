a40bccd488a6fdc5a24492b39f7e8ed7b056c736
Marco Lodovici <marcolodo98@gmail.com>
2025-05-12 11:43:17 +0200

    refactor(actionLogin): reorder imports and change redirect destination to HOME

--- src/router/actions/auth/actionLogin.ts
+++ src/router/actions/auth/actionLogin.ts
@@ -1,9 +1,9 @@
-import { redirect } from 'react-router-dom';
+import NetworkClient from '@/api/v1/NetworkClient.ts';
+import { ApiRequestLogin } from '@/api/v1/requests/ApiRequestLogin';
 import { EnumRoutes } from '@/models/enums/EnumRoutes';
 import { AuthContextType } from '@/providers/AuthProvider.tsx';
-import NetworkClient from '@/api/v1/NetworkClient.ts';
 import { NotificationContextType } from '@/providers/NotificationProvider';
-import { ApiRequestLogin } from '@/api/v1/requests/ApiRequestLogin';
+import { redirect } from 'react-router-dom';
 
 const actionLogin =
   ({ login }: AuthContextType, { toast }: NotificationContextType) =>
@@ -21,7 +21,7 @@ const actionLogin =
 
       const url = new URL(request.url);
       const redirectTo: string | null = url.searchParams.get('redirect');
-      return redirect(redirectTo || EnumRoutes.DASHBOARD);
+      return redirect(redirectTo || EnumRoutes.HOME);
     } catch (error) {
       console.error('error: ', error);
       toast({
