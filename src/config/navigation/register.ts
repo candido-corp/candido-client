14410a3aa9ae573d550a94cdf2777019ac27d98e
Marco Lodovici <marcolodo98@gmail.com>
2025-05-12 18:32:19 +0200

    CC-27 refactor(navigation): improve navigation plugin registration and enhance navbar item retrieval based on authentication status

--- src/config/navigation/register.ts
+++ src/config/navigation/register.ts
@@ -7,6 +7,7 @@ const plugins: NavigationPlugin[] = [];
 
 /**
  * Registers a navigation plugin (navbar + sidebar).
+ * @param plugin
  */
 export const registerNavigationPlugin = (plugin: NavigationPlugin) => {
   if (!plugin.id) throw new Error('Navigation plugin must have an id');
@@ -17,45 +18,59 @@ export const registerNavigationPlugin = (plugin: NavigationPlugin) => {
 };
 
 /**
- * Returns all registered navbar entries in config order.
- */
-export const getNavbarItems = (isUserAuthenticated: boolean): FlatNavItem[] =>
-  NAVIGATION_CONFIG.map(
-    ({ id }) => plugins.find((p) => p.id === id)?.navbar
-  ).filter(
-    (item): item is FlatNavItem =>
-      item !== undefined &&
-      item.visibility !== undefined &&
-      item.visibility?.indexOf(EnumNavigationVisibility.DESKTOP) > -1 &&
-      (item.needsAuth === isUserAuthenticated || item.needsAuth === false)
-  );
-
-/**
  * Returns all plugins visible in config order.
+ * if isUserAuthenticated is undefined, it returns all plugins.
+ * if isUserAuthenticated is true, it returns all plugins that need authentication.
+ * if isUserAuthenticated is false, it returns all plugins that do not need authentication.
+ * @param isUserAuthenticated
+ * @returns {NavigationPlugin[]}
  */
-export const getNavigationPlugins = (): NavigationPlugin[] =>
+export const getNavigationPlugins = (
+  isUserAuthenticated?: boolean
+): NavigationPlugin[] =>
   NAVIGATION_CONFIG.map(({ id }) => plugins.find((p) => p.id === id)).filter(
     (p): p is NavigationPlugin =>
-      p !== undefined && p.navbar.visibility !== undefined
-  );
-
-/**
- * Returns all plugins with mobile visibility in config order.
- */
-export const getMobileNavigationPlugins = (
-  isUserAuthenticated: boolean
-): NavigationPlugin[] =>
-  getNavigationPlugins().filter(
-    (p) =>
-      p.navbar &&
-      p.navbar.visibility &&
-      p.navbar.visibility?.indexOf(EnumNavigationVisibility.MOBILE) > -1 &&
-      (p.navbar.needsAuth === isUserAuthenticated ||
+      p !== undefined &&
+      p.navbar.visibility !== undefined &&
+      (isUserAuthenticated === undefined ||
+        p.navbar.needsAuth === isUserAuthenticated ||
         p.navbar.needsAuth === false)
   );
 
 /**
+ * Returns all registered navbar entries in config order.
+ * @param isUserAuthenticated
+ * @returns {FlatNavItem[]}
+ */
+export const getNavbarItems = (isUserAuthenticated: boolean): FlatNavItem[] =>
+  getNavigationPlugins(isUserAuthenticated)
+    .map((plugin) => plugin.navbar)
+    .filter(
+      (item): item is FlatNavItem =>
+        item !== undefined &&
+        item.visibility !== undefined &&
+        item.visibility?.indexOf(EnumNavigationVisibility.DESKTOP) > -1
+    );
+
+/**
+ * Returns all plugins with mobile visibility in config order.
+ * @param isUserAuthenticated
+ * @returns {NavigationPlugin[]}
+ */
+export const getMobileNavigationPlugins = (
+  isUserAuthenticated: boolean
+): NavigationPlugin[] =>
+  getNavigationPlugins(isUserAuthenticated).filter(
+    (p) =>
+      p.navbar &&
+      p.navbar.visibility &&
+      p.navbar.visibility?.indexOf(EnumNavigationVisibility.MOBILE) > -1
+  );
+
+/**
  * Returns a specific navigation plugin by pluginId.
+ * @param pluginId
+ * @returns {NavigationPlugin | undefined}
  */
 export const getCurrentPlugin = (
   pluginId: EnumNavigationPlugin
